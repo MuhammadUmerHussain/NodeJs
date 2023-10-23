030da9a6f5cc8168a79e36453a83db47926b94c7
MuhammadUmerHussain <umerhussain231123@gmail.com>
2023-10-23 08:16:25 -0700

    4:30

--- node-express-course-main/04-store-api/starter/controllers/products.js
+++ node-express-course-main/04-store-api/starter/controllers/products.js
@@ -1,8 +1,36 @@
+const Products=require('../models/product')
+
+
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "hello from get all products" });
+
+ const results=await Products.find({})
+  res.status(200).json({ results });
 };
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "hello from static" });
+
+  const {featured,name,company,sort}=req.query
+  const queryObjects={}
+  if(featured){
+    queryObjects.featured=featured=='true'?true:false;
+  }
+  if(name){
+    console.log(name,"name");
+    queryObjects.name={$regex:name, $options :'i'};
+  }
+  if(company){
+    queryObjects.company=company
+  }
+  
+  console.log(queryObjects);
+  let result=  Products.find(queryObjects)
+  if(sort){
+    console.log(result)
+   const sortlist=sort.split(',').join(' ');
+   result=result.sort(sortlist)
+      
+  }
+  const products= await result
+  res.status(200).json({products,nbHits:products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
